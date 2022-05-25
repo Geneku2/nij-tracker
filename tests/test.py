695ef562edad507365a54902738114a131281e65
Hao Qi <49293828+Geneku2@users.noreply.github.com>
2022-05-25 15:48:39 -0400

    refined code for future work

--- tests/test.py
+++ tests/test.py
@@ -12,18 +12,14 @@ import keys
 youtube = build("youtube", "v3", developerKey=keys.api_key)
 request = youtube.channels().list(
     part="snippet",
-    #id="UCS9uQI-jC3DE0L4IpXyvr6w"              #kiryu coco by id
-    #forUsername = 'Coco Ch. 桐生ココ'           #kiryu coco by username
-    #forUsername = "3y0iPFR93VoEzeFcrB5mcA"     #my dumb ass
-    #forUsername = "Gigguk"                     #the biggus giggus
-    id = "UC7dF9qfBMXrSlaaFFDvV_Yg"            #the biggus giggus id
-    #forUsername = "Niconiconii"                         #ed dan DOESN'T WORK
-    #id = "UCH-NXU_A3HmisP1cM1rkm6Q"            #ed dan id
+    #forUsername = "Gigguk"                     # Gigguk's Username (only legacy channels have usernames)
+    #id = "animezone"                           # Gigguk's Creator Tag (tags DO NOT WORK as id NOR username so it is USELESS)
+    id = "UC7dF9qfBMXrSlaaFFDvV_Yg"            # Gigguk's channelid (found in the HTML)
 
 )
 
 response = request.execute()
 
 print(response)
-print("\n")
-#print(response["items"][0]["snippet"]["title"])
+print()
+print(response["items"][0]["snippet"]["title"])
