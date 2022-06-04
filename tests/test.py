1d248f99dd9155208ca9048a588fb721015f8437
Hao Qi <49293828+Geneku2@users.noreply.github.com>
2022-06-04 17:34:46 -0400

    add demo csv for rendering main.py and parser, touched up "test" for later production

--- tests/test.py
+++ tests/test.py
@@ -8,18 +8,17 @@ import sys
 sys.path.insert(0, './')
 import keys
 
+#forUsername = "Gigguk"                     # Gigguk's Username (only legacy channels have usernames)
+#id = "animezone"                           # Gigguk's Creator Tag (tags DO NOT WORK as id NOR username so it is USELESS)
+myid = "UC7dF9qfBMXrSlaaFFDvV_Yg"            # Gigguk's channelid (found in the HTML)
+
 
 youtube = build("youtube", "v3", developerKey=keys.api_key)
-request = youtube.channels().list(
-    part="snippet",
-    #forUsername = "Gigguk"                     # Gigguk's Username (only legacy channels have usernames)
-    #id = "animezone"                           # Gigguk's Creator Tag (tags DO NOT WORK as id NOR username so it is USELESS)
-    id = "UC7dF9qfBMXrSlaaFFDvV_Yg"            # Gigguk's channelid (found in the HTML)
+snippetRequest = youtube.channels().list(part="snippet", id = myid)
+statsRequest = youtube.channels().list(part="statistics", id = myid)
 
-)
+username = snippetRequest.execute()["items"][0]["snippet"]["title"]
+subcount = statsRequest.execute()["items"][0]["statistics"]["subscriberCount"]
 
-response = request.execute()
-
-print(response)
-print()
-print(response["items"][0]["snippet"]["title"])
+print(username)
+print(subcount)
