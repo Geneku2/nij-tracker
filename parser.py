0cf7ab0a9f3cc9cbeb55c26c4fa359b610912c2c
Hao Qi <49293828+Geneku2@users.noreply.github.com>
2022-06-13 19:58:51 -0400

    implement youtube channel id fetching
    given a nijisanji website readable format, the function fetch_yt_id accesses the page of the liver on the NIJI website then parses that page to isolate the channel id. tested for EN and JP, but functionality is questionable on other devices

--- parser.py
+++ parser.py
@@ -1,3 +1,4 @@
+import string
 from unicodedata import name
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
@@ -53,35 +54,96 @@ def get_members(branch = "en"):
                 names[nameIdx] = names[nameIdx][:names[nameIdx].index("%20")] + "-" + names[nameIdx][names[nameIdx].index("%20")+3:]
         names[nameIdx] = names[nameIdx].lower()
 
+        #nijisanji, please fix your naming, pleaseee
+        if names[nameIdx] == "riri-yuuhi":
             #yuuhi = yuhi
-            #Aduchi = Azuchi
-            #Uduki = Uzuki
-            #Shouichi = Shoichi
+            names[nameIdx] = "riri-yuhi"
+        elif names[nameIdx] == "yoko-akabane":
+            #yoko = youko
+            names[nameIdx] = "youko-akabane"
+        elif names[nameIdx] == "momo-aduchi":
+            #aduchi = azuchi
+            names[nameIdx] = "momo-azuchi"
+        elif names[nameIdx] == "kou-uduki":
+            #uduki = uzuki
+            names[nameIdx] = "kou-uzuki"
+        elif names[nameIdx] == "ririmu-makaino":
+            #rirmu makaino = makaino ririmu
+            names[nameIdx] = "makaino-ririmu"
+        elif names[nameIdx] == "shouichi-kanda":
+            #shouichi = shoichi
+            names[nameIdx] = "shoichi-kanda"
+        elif names[nameIdx] == "mirei-gundou":
             #gundou = gundo
-
-
-
-    #    for characterIdx in range(len(names[nameIdx])):
-    #        if(names[nameIdx][characterIdx] == '_'):
-    #            names[nameIdx] = names[nameIdx][0:characterIdx] + names[nameIdx][characterIdx+1:len(names[nameIdx])]
-    #            break
-
-    #for nameIdx in range(len(names)):
-    #    numUpper = 0
-    #    for characterIdx in range(len(names[nameIdx])):
-    #        if(names[nameIdx][characterIdx].isupper()):
-    #            numUpper+=1
-    #        if numUpper > 1:
-    #            names[nameIdx] = names[nameIdx][0:characterIdx] + "-" + names[nameIdx][characterIdx:len(names[nameIdx])]
-    #            break
-    #    names[nameIdx] = names[nameIdx].lower()
+            names[nameIdx] = "mirei-gundo"
+        elif names[nameIdx] == "suha-min":
+            #suha min = min suha
+            names[nameIdx] = "min-suha"        
+        elif names[nameIdx] == "nagi-so":
+            #nagi so = so nagi
+            names[nameIdx] = "so-nagi"    
+        elif names[nameIdx] == "roha-lee":
+            #roha lee = lee roha
+            names[nameIdx] = "lee-roha"       
+        elif names[nameIdx] == "ray-akira":
+            #ray akira = akira ray
+            names[nameIdx] = "akira-ray"     
+        elif names[nameIdx] == "jiyu-oh":
+            #jiyu oh = oh jiyu
+            names[nameIdx] = "oh-jiyu"     
+        elif names[nameIdx] == "nari-yang":
+            #nari yang = yang nari
+            names[nameIdx] = "yang-nari"
+        elif names[nameIdx] == "hari-ryu":
+            #hari ryu = ryu hari
+            names[nameIdx] = "ryu-hari"         
+          
     return names
 
+def fetch_yt_id(livername):
+    #in case wrong format, should take liver name as parsed string
+    if not isinstance(livername, str):
+        raise Exception(str(livername) + " Is Not a Valid Liver Name")
+
+    #convert to nijisanji page url
+    url = "https://www.nijisanji.jp/en/members/" + livername
+
+
+    #beautiful soup shit
+    this_session = HTMLSession()
+    request = this_session.get(url)
+    request.html.render()
+
+    soup = BeautifulSoup(request.html.raw_html, "html.parser")
+    div_containers = soup.findAll("a", attrs={"target":"_blank","rel":"noopener noreferrer"})
+    channelID = "none"
+    
+    #find youtube target reference element
+    for item in div_containers:
+        if str(item["href"]).find("youtube") != -1:
+            channelID = str(item["href"])
+            break
+
+    #if liver not found, then channelID is null and throws exception
+    if channelID == "none":
+        raise Exception("There Is No Liver Named " + livername + " In NIJISANJI")
+
+    #more string parsing to isolate channel ID for YTv3API
+    qMarkIdx = len(channelID)-1
+    for characterIdx in range(len(channelID)-1, -1, -1):
+        if channelID[characterIdx] == '?':
+            qMarkIdx = characterIdx
+        if channelID[characterIdx] == '/':
+            channelID = channelID[characterIdx + 1:qMarkIdx]
+            break
+
+    return channelID
+    
+
 list = get_members(branch="jp")
-list2 = get_members(branch="en")
+for mem in list:
+    print(fetch_yt_id(mem))
 
-for item in list:
-    print(item)
-
-for item in list2:
-    print(item)
+list = get_members(branch="en")
+for mem in list:
+    print(fetch_yt_id(mem))
